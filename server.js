cc712fb9c33f0ed267bcfcc2c46ec756bfecf71c
benmatiko <maroamatiko@gmail.com>
2025-12-26 13:17:01 +0300

    mongodb setup

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
+
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
 const path = require('path')
 const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 
 const PORT = process.env.PORT || 3500
 
+console.log(process.env.NODE_ENV)
+
 app.use(logger )
+app.use(cors(corsOptions
 
+
+))
 app.use(express.json())
-
+app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
-
 app.all(/.*/, (req, res) => {
   res.status(404)
   if (req.accepts('html')) {
@@ -38,7 +47,7 @@ app.all(/.*/, (req, res) => {
 
 
 
-
+app.use(errorHandler)
 // Tell app to liste to specific port
 app.listen(PORT, () => console.log(`Server running on port  + ${PORT}`))
 
